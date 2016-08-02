ac07cb3883d10e763cccf0a9e36e985cdc52d6c1
mfurquan <furquan2011@gmail.com>
2016-08-02 22:38:33 +0530

    polynomial tested

--- tests/polynomial/main.f90
+++ tests/polynomial/main.f90
@@ -3,16 +3,33 @@ program test_polynomial
    use polynomial_type
    implicit none
 
-   type(polynomial) :: p
+   type(polynomial) :: p, dp
    real(kind=wp) :: a(3), c
-   integer :: e(1,3)
-   character(len=var_len) :: x(1)
+   integer :: e(1,3), e1(2,3)
+   character(len=var_len) :: x(1), x1(2)
 
-   a = [5, 1, 7.2]
-   c = 5
-   e = [3, 2, 1]
+   a = [5.0, 1.0, 7.2]
+   c = 5.0
+   e(1,:) = [3, 2, 1]
    x = 'x'
 
-   call p%set_poly(a,e,x,c)
+   call p%set_polynomial(a,e,x,c)
    call p%prnt
-end program polynomial
+   dp=p%deriv('x')
+   call dp%prnt
+
+   e1(1,:) = e(1,:)
+   e1(2,:) = [2, 1, 0]
+   x1      = ['x', 'y']
+
+   call p%set_polynomial(a,e1,x1,c)
+   call p%prnt
+   dp=p%deriv('x')
+   call dp%prnt
+   dp=p%deriv('y')
+   call dp%prnt
+   dp=dp%deriv('x')
+   call dp%prnt
+
+   write(*,*) 'dp(3,2)=',dp%eval([3.0_wp,2.0_wp])
+end program test_polynomial
